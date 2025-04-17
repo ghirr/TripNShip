7ebb800923793a7ed3f068b3cf9ab2ae00a9f048
Islem Ghariani <islemghariani.dev@gmail.com>
2025-04-17 08:54:08 +0100

    Making the login and signup form dynamic

--- src/main/java/org/Esprit/TripNShip/Services/AuthService.java
+++ src/main/java/org/Esprit/TripNShip/Services/AuthService.java
@@ -6,31 +6,53 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.time.LocalDateTime;
+import java.util.Date;
 
 import org.Esprit.TripNShip.Entities.Client;
+import org.Esprit.TripNShip.Entities.Gender;
 import org.Esprit.TripNShip.Entities.Role;
 import org.Esprit.TripNShip.Entities.User;
 import org.Esprit.TripNShip.Utils.MyDataBase;
 import org.mindrot.jbcrypt.BCrypt;
 
+import static org.Esprit.TripNShip.Utils.Shared.getEnumOrNull;
+
 public class AuthService {
     private Connection cnx = MyDataBase.getInstance().getConnection();
 
     public User login(String email, String password) {
-        String query = "SELECT id, email, password, role FROM user WHERE email = ?";
+        String query = "SELECT id, email, password, role,firstName,lastName,gender,profilePhoto,birthDayDate,phoneNumber FROM user WHERE email = ?";
         try (PreparedStatement pst = cnx.prepareStatement(query)) {
             pst.setString(1, email);
 
             ResultSet resultSet = pst.executeQuery();
 
             if (resultSet.next()) {
+
+                LocalDateTime birthDateTime = null;
+                Date birthDate = resultSet.getDate("birthDayDate");
+                if (birthDate != null) {
+                    birthDateTime = resultSet.getDate("birthDayDate").toLocalDate().atStartOfDay();
+                }
+
+
                 User user = new User(
                         resultSet.getInt("id"),
+                        resultSet.getString("firstName"),
+                        resultSet.getString("lastName"),
+                        getEnumOrNull(Gender.class, resultSet.getString("gender")),
+                        getEnumOrNull(Role.class, resultSet.getString("role")),
                         resultSet.getString("email"),
-                        resultSet.getString("password"),
-                        Role.valueOf(resultSet.getString("role")) // Convertit la chaîne de la BD en Role ENUM
+                        resultSet.getString("profilePhoto"),
+                        birthDateTime,
+                        resultSet.getString("phoneNumber")// Convertit la chaîne de la BD en Role ENUM
                 );
-                if(BCrypt.checkpw(password, user.getPassword())) {
+                String passwordBD = resultSet.getString("password");
+                if (passwordBD == null || passwordBD.isEmpty()) {
+                    return null;
+                }
+                if(BCrypt.checkpw(password, passwordBD)) {
                     return user;
                 }
             }
@@ -40,8 +62,27 @@ public class AuthService {
         return null; // Si aucun utilisateur trouvé
     }
 
+    public void signUp(Client client) {
+        String req = "INSERT INTO User (\n" +
+                "    firstName, lastName, role, email, password)"+
+                " VALUES (?,?,?,?,?)";
+        try {
+            PreparedStatement ps = cnx.prepareStatement(req);
+            ps.setString(1, client.getFirstName());
+            ps.setString(2, client.getLastName());
+            ps.setString(3, client.getRole().toString());
+            ps.setString(4, client.getEmail());
+            System.out.println(client.getPassword());
+            ps.setString(5, BCrypt.hashpw(client.getPassword(), BCrypt.gensalt(10)));
+            ps.executeUpdate();
+            System.out.println("Client signed up !");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
     public User googleLogin(String email) {
-        String query = "SELECT firstName,lastName,role,profilePhoto FROM user WHERE email = ?";
+        String query = "SELECT id,firstName,lastName,role,profilePhoto FROM user WHERE email = ?";
         try (PreparedStatement pst = cnx.prepareStatement(query)) {
             pst.setString(1, email);
 
@@ -49,11 +90,12 @@ public class AuthService {
 
             if (resultSet.next()) {
                 User user = new User(
-                        resultSet.getString(1),
+                        resultSet.getInt(1),
                         resultSet.getString(2),
-                        Role.valueOf(resultSet.getString(3)),
+                        resultSet.getString(3),
+                        Role.valueOf(resultSet.getString(4)),
                         email,
-                        resultSet.getString(4)
+                        resultSet.getString(5)
                 );
 
                 return user;
--- src/main/java/org/Esprit/TripNShip/Utils/Shared.java
+++ src/main/java/org/Esprit/TripNShip/Utils/Shared.java
@@ -7,8 +7,10 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
+import javafx.scene.control.DialogPane;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.io.IOException;
 import java.net.URL;
@@ -36,4 +38,31 @@ public class Shared {
             //showAlert(Alert.AlertType.ERROR, "Erreur", "Impossible de charger la page : " + fxmlFile);
         }
     }
+
+    public static void showAlert(Alert.AlertType type, String title, String message) {
+        Alert alert = new Alert(type);
+        alert.setTitle(title);
+        alert.setHeaderText(null); // Remove header text
+        alert.setContentText(message);
+
+        // Apply CSS
+        DialogPane dialogPane = alert.getDialogPane();
+//        dialogPane.getStylesheets().add(getClass().getResource("/Styles/custom-alert.css").toExternalForm());
+
+        // Make window draggable
+        alert.initStyle(StageStyle.TRANSPARENT);
+
+        // The OK button will already be styled by the CSS
+
+        alert.showAndWait();
+    }
+
+    public static <T extends Enum<T>> T getEnumOrNull(Class<T> enumType, String value) {
+        try {
+            return value != null ? Enum.valueOf(enumType, value) : null;
+        } catch (IllegalArgumentException e) {
+            return null;
+        }
+    }
+
 }

db28343bbfbc6a64b66445b7f9cdc21eb15f4156
IyedABDELLI <mohamediyed.abdelli@esprit.tn>
2025-04-25 01:57:59 +0100

    Combolist added to Add Transport Interface

--- src/main/java/org/Esprit/TripNShip/Controllers/AddTransportController.java
+++ src/main/java/org/Esprit/TripNShip/Controllers/AddTransportController.java
@@ -1,7 +1,9 @@
 package org.Esprit.TripNShip.Controllers;
 
+import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.*;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
@@ -11,20 +13,27 @@ import org.Esprit.TripNShip.Entities.TransportType;
 import org.Esprit.TripNShip.Services.TransportService;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class AddTransportController {
+public class AddTransportController implements Initializable {
     @FXML private TextField transporterReferenceField;
-    @FXML private TextField transportationField;
+    @FXML ComboBox<TransportType> transportationComboBox;
+
     @FXML private TextField companyNameField;
     @FXML private TextField companyPhoneField;
     @FXML private TextField companyEmailField;
     @FXML private TextField companyWebsiteField;
     @FXML private Button addTransportButton;
 
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        transportationComboBox.setItems(FXCollections.observableArrayList(TransportType.values()));
+    }
     @FXML
     void addTransport(ActionEvent event) throws IOException{
         TransportService ts = new TransportService();
-        ts.add(new Transport(transporterReferenceField.getText(),TransportType.valueOf(transportationField.getText()),companyNameField.getText(),Integer.parseInt(companyPhoneField.getText()),companyEmailField.getText(),companyWebsiteField.getText()));
+        ts.add(new Transport(transporterReferenceField.getText(),transportationComboBox.getValue(),companyNameField.getText(),Integer.parseInt(companyPhoneField.getText()),companyEmailField.getText(),companyWebsiteField.getText()));
 
         Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
         alert.setTitle("SA7it");

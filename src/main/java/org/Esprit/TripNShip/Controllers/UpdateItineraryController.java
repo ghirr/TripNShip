5f22994c7c57971bc6b9d5a036f908c46247d69f
IyedABDELLI <mohamediyed.abdelli@esprit.tn>
2025-05-30 18:03:35 +0100

    replacing time textfield with time spinner in itinerary add/update

--- src/main/java/org/Esprit/TripNShip/Controllers/UpdateItineraryController.java
+++ src/main/java/org/Esprit/TripNShip/Controllers/UpdateItineraryController.java
@@ -1,21 +1,25 @@
 package org.Esprit.TripNShip.Controllers;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Alert;
-import javafx.scene.control.Button;
-import javafx.scene.control.TextField;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
 import javafx.stage.Stage;
+import javafx.util.StringConverter;
 import org.Esprit.TripNShip.Entities.Itinerary;
 import org.Esprit.TripNShip.Services.ItineraryService;
 
+import java.net.URL;
 import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ResourceBundle;
 
-public class UpdateItineraryController {
+public class UpdateItineraryController implements Initializable {
 
     @FXML private TextField itineraryCodeField;
     @FXML    private TextField transporterReferenceField;
     @FXML    private TextField departureLocationField;
-    @FXML private TextField departureTimeField;
+   // @FXML private TextField departureTimeField;
+   @FXML private Spinner<LocalTime> departureTimeSpinner;
     @FXML    private TextField arrivalLocationField;
     @FXML    private TextField durationField;
     @FXML    private TextField priceField;
@@ -29,8 +33,8 @@ public class UpdateItineraryController {
         itineraryCodeField.setText(itinerary.getItineraryCode());
         transporterReferenceField.setText(itinerary.getTransporterReference());
         departureLocationField.setText(itinerary.getDepartureLocation());
-        departureTimeField.setText(itinerary.getDepartureTime().toString());
         arrivalLocationField.setText(itinerary.getArrivalLocation());
+        departureTimeSpinner.getValueFactory().setValue(itinerary.getDepartureTime());
         durationField.setText(itinerary.getDuration());
         priceField.setText(Double.toString(itinerary.getPrice()));
     }
@@ -41,7 +45,7 @@ public class UpdateItineraryController {
         itinerary.setItineraryCode(itineraryCodeField.getText());
         itinerary.setTransporterReference(transporterReferenceField.getText());
         itinerary.setDepartureLocation((departureLocationField.getText()));
-        itinerary.setDepartureTime(LocalTime.parse(departureTimeField.getText()));
+        itinerary.setDepartureTime(departureTimeSpinner.getValue());
         itinerary.setArrivalLocation(arrivalLocationField.getText());
         itinerary.setDuration((durationField.getText()));
         itinerary.setPrice(Double.parseDouble(priceField.getText()));
@@ -58,7 +62,7 @@ public class UpdateItineraryController {
         stage.close();
     }
     public boolean validInputs() {
-        if (itineraryCodeField.getText().isEmpty() ||  transporterReferenceField.getText().isEmpty() || departureLocationField.getText().isEmpty() || departureTimeField.getText().isEmpty()||arrivalLocationField.getText().isEmpty()||durationField.getText().isEmpty()||priceField.getText().isEmpty()) {
+        if (itineraryCodeField.getText().isEmpty() ||  transporterReferenceField.getText().isEmpty() || departureLocationField.getText().isEmpty() || departureTimeSpinner.getValue()==null||arrivalLocationField.getText().isEmpty()||durationField.getText().isEmpty()||priceField.getText().isEmpty()) {
             showAlert(Alert.AlertType.ERROR, "Validation Error", "Please Fill in all required fields");
             return false;
 
@@ -74,12 +78,12 @@ public class UpdateItineraryController {
             showAlert(Alert.AlertType.ERROR,"Price Format Error","Please set the price as Double");
             return false;
         }
-        try {
+        /*try {
             LocalTime.parse(departureTimeField.getText());
         } catch (Exception e) {
             showAlert(Alert.AlertType.ERROR,"Time Format Error","Please set the time in the format HH:MM");
             return false;
-        }
+        }*/
 
         return true;
     }
@@ -89,5 +93,40 @@ public class UpdateItineraryController {
         alert.setTitle(title);
         alert.setContentText(message);
         alert.showAndWait();
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        SpinnerValueFactory<LocalTime> timeFactory = new SpinnerValueFactory<>() {
+            {
+                setConverter(new StringConverter<>() {
+                    final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm");
+
+                    @Override
+                    public String toString(LocalTime time) {
+                        return time != null ? formatter.format(time) : "";
+                    }
+
+                    @Override
+                    public LocalTime fromString(String string) {
+                        return LocalTime.parse(string, formatter);
+                    }
+                });
+
+                setValue(LocalTime.of(8, 0)); // Valeur par défaut
+            }
+
+            @Override
+            public void decrement(int steps) {
+                setValue(getValue().minusMinutes(steps * 05));
+            }
+
+            @Override
+            public void increment(int steps) {
+                setValue(getValue().plusMinutes(steps * 05));
+            }
+        };
+
+        departureTimeSpinner.setValueFactory(timeFactory);
     }
 }
